131ed83091b86e931e9a54ac7acb299d37d44be7
Tokigin <tokigin321@gmail.com>
2023-10-31 11:13:11 +0630

    format

--- Cannel/Router.php
+++ Cannel/Router.php
@@ -1,5 +1,4 @@
 <?php
-
 class Router
 {
     public static $Root = "/php-router"; // project name (remove if deploy to server eg."/")
@@ -7,14 +6,13 @@ class Router
     public static $Extention = ".php";
     public static $Checkdir = false;
     public static $Return_404 = true;
-
     public static function Handle()
     {
         $root = self::$Root;
         $dir_page = self::$Dir_page;
         $ext = self::$Extention;
         $currentUri = $_SERVER["REQUEST_URI"];
-        Self::CurrentUri($dir_page, $currentUri, $ext, $root);
+        self::CurrentUri($dir_page, $currentUri, $ext, $root);
         $checkdir = self::$Checkdir;
         $return_404 = self::$Return_404;
         if ($currentUri === "$root/") {
