9a79ba179084c7b45ad14e000f0845c651140a7d
Tokigin <134780921+Tokigin@users.noreply.github.com>
2024-08-29 13:54:32 +0700

    fix layout

--- Cannel/Layout.php
+++ Cannel/Layout.php
@@ -5,18 +5,10 @@ class Layout
     public static bool $Footer = true;
     public static function Headerlayout(bool $header): void
     {
-        if ($header) {
-            if (file_exists("./Layout/Header.php")) {
-                require "./Layout/Header.php";
-            } else ErrorText::Show('"Header.php" not found. Create Header.php file in ./Layout or use "Layout::$Header = false;" in Setting.php');
-        }
+        if ($header) file_exists("./Layout/Header.php") ? require "./Layout/Header.php" : self::$Header = false;
     }
     public static function Footerlayout(bool $footer): void
     {
-        if ($footer) {
-            if (file_exists("./Layout/Footer.php")) {
-                require "./Layout/Footer.php";
-            } else ErrorText::Show('"Footer.php" not found. Create Header.php file in ./Layout or use "Layout::$Footer = false;" in Setting.php');
-        }
+        if ($footer) file_exists("./Layout/Footer.php") ?  require "./Layout/Footer.php" : self::$Footer = false;
     }
 }
