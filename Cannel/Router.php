e44594209afabed62d1188913b6825d1ea59aa58
Tokigin <134780921+Tokigin@users.noreply.github.com>
2024-09-10 18:05:30 +0700

    sub route

--- Cannel/Router.php
+++ Cannel/Router.php
@@ -7,24 +7,49 @@ class Router
     private static bool $Check_dir = false;
     private static string $Dir_page = "Pages";
     private static bool $Return_404 = true;
+    private static bool $Sub_Folder = false;
     public static function AutoRouter(): void
     {
         $root = self::$Root;
         $dir_page = self::$Dir_page;
         $ext = self::$Extension;
         $index = self::$Home_Page;
-        self::CheckPage($dir_page, $_SERVER["REQUEST_URI"], $ext, $root);
+        self::CheckSub();
+        self::CheckPage(self::$Dir_page, $_SERVER["REQUEST_URI"], $ext, $root);
+        self::Index_Fetching($root, $dir_page, $index, $ext);
+        self::Page_Fetching($root, $dir_page, $ext);
+        if (self::$Sub_Folder && self::$Return_404) {
+            $sub_folders = self::GetSub();
+            foreach ($sub_folders as $subfolder) {
+                self::CheckPage($dir_page . $subfolder, $_SERVER["REQUEST_URI"], $ext, $root);
+                echo $_SERVER["REQUEST_URI"] . "<br>";
+                echo $dir_page . $subfolder . "<br>";
+            }
+
+            self::Index_Fetching($root, $dir_page . $subfolder, $index, $ext);
+            self::Page_Fetching($root, $dir_page . $subfolder, $ext);
+        }
+
+        if (self::$Return_404) self::Return404();
+    }
+    private static function Index_Fetching(string $root, string $dir_page, string $index, string $ext): void
+    {
         if ($_SERVER["REQUEST_URI"] === "$root/") {
             if (file_exists("./$dir_page/$index$ext")) {
                 require_once "./$dir_page/$index$ext";
                 self::$Return_404 = false;
             } else ErrorText::Show("(Using Auto Router) $index$ext file not found in source code. Check the file dictionary.");
         }
-        if (self::$Check_dir) require_once str_replace($root, "$dir_page", $_SERVER["REQUEST_URI"]) . "$ext";
-        if (self::$Return_404) self::Return404();
+    }
+    private static function Page_Fetching(string $root, string $dir_page, string $ext): void
+    {
+        if (self::$Check_dir) {
+            require_once str_replace($root, "$dir_page", $_SERVER["REQUEST_URI"]) . "$ext";
+        }
     }
     private static function CheckPage(string $dir_page, string $currentUri, string $ext, string $root): void
     {
+        //need to fix
         foreach (glob("$dir_page/*$ext") as $file) {
             $page = str_replace("$dir_page/", "", $file);
             if ("$currentUri$ext" === "$root/$page") {
@@ -33,6 +58,7 @@ class Router
             }
         }
     }
+
     public static function CurrentPage(string $filename): bool
     {
         return $_SERVER["REQUEST_URI"] === self::$Root . "/$filename" ? true : false;
@@ -59,5 +85,18 @@ class Router
         $dir_page = self::$Dir_page;
         $index = self::$Home_Page;
         file_exists("./Layout/404$ext") ? require_once "./Layout/404$ext" : require_once "./$dir_page/$index$ext";
+    }
+
+    private static function GetSub()
+    {
+        $subfolders = glob('./Pages/*', GLOB_ONLYDIR);
+        foreach ($subfolders as $subfolder) {
+            $array[] = str_replace('./Pages', '', $subfolder);
+        }
+        return $array;
+    }
+    private static function CheckSub()
+    {
+        (count(glob('./Pages/*', GLOB_ONLYDIR)) > 0) ? self::$Sub_Folder = true : self::$Sub_Folder = false;
     }
 }
--- Sections/hero-banner.php
+++ Sections/hero-banner.php
@@ -3,7 +3,7 @@
         <div class=" row justify-content-md-center align-items-center justify-content-center " style="height: 90vh;">
             <div class=" col-12 col-md-11 col-lg-9 col-xl-7 col-xxl-6 text-center text-white">
                 <h1 class="display-3 fw-bold mb-3"><?php echo $title ?></h1>
-                <p class="lead mb-5">Developed by Ko Htet. This framework is made for simple information website development for beginner. 😁 </p>
+                <p class="lead mb-5">Developed by Ko Htet. This framework is made for simple information website development for beginner. </p>
                 <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
                     <a href="https://github.com/Tokigin/php-router?tab=readme-ov-file#php-router-cannel" class="btn sb-btn-3xl btn-light gap-3">Framework Guide</a>
                     <a href="#" class="btn bsb-btn-3xl btn-outline-light">Learn more</a>
